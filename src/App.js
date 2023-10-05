ab2864af398b7be3b1c7a5966638eeddad228c38
kanae95v <127657026+kanae95v@users.noreply.github.com>
2023-10-05 20:27:05 +0300

    modified:   package-lock.json     	modified:   package.json     	new file:   public/img/images-ctul.jpeg     	modified:   src/App.js     	new file:   src/components/Footer.js     	new file:   src/components/Header.js     	new file:   src/components/Item.js     	new file:   src/components/Items.js     	new file:   src/components/Order.js     	new file:   src/img/foto-header.jpeg     	modified:   src/index.css     	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Items from "./components/Items";
 
-function App() {
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      orders: [],
+      items: [
+        {
+          id:1,
+          title:'Стул серый',
+          img:'images-ctul.jpeg',
+          desc:'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          categoru: 'chairs',
+          price:'12000'
+        },
+        {
+          id:2,
+          title:'Стол',
+          img:'images-ctul.jpeg',
+          desc:'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          categoru: 'chairs',
+          price:'49000'
+        },
+        {
+          id:3,
+          title:'Диван',
+          img:'images-ctul.jpeg',
+          desc:'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          categoru: 'chairs',
+          price:'150000'
+        },
+        {
+          id:4,
+          title:'Лампа',
+          img:'images-ctul.jpeg',
+          desc:'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          categoru: 'chairs',
+          price:'8000'
+        },        {
+          id:5,
+          title:'Стул белый',
+          img:'image-lampa2.jpeg',
+          desc:'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          categoru: 'chairs',
+          price:'12000'
+        },
+        {
+          id:6,
+          title:'Стул деревянный',
+          img:'images-ctul.jpeg',
+          desc:'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          categoru: 'chairs',
+          price:'13000'
+        }
+      ]
+    }
+    this.addToOrder = this.addToOrder.bind(this)
+    this.deleteOrder = this.deleteOrder.bind(this)
+  }
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header orders={this.state.orders} onDelete={this.deleteOrder}/>
+      <Items items={this.state.items} onAdd={this.addToOrder}/>
+      <Footer/>
     </div>
-  );
+  )
+  }
+
+  deleteOrder(id) {
+    this.setState({orders: this.state.orders.filter(el => el.id !== id)})
+  }
+
+  addToOrder(item) {
+      this.setState({orders: [...this.state.orders, item]})
+  }
 }
 
 export default App;
